c7447bb85d78d8948417f96e3b57891c77953d57
JPro81 <60090059+JPro81@users.noreply.github.com>
2021-09-16 15:59:27 +0200

    druhy pokus

---
+++ ClassLibrary1/ClassLibrary1/PersonPrinter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class PersonPrinter
+    {
+        public PersonPrinter(Person person)
+        {
+            Person = person ?? throw new ArgumentNullException(nameof(person));
+        }
+
+        public Person Person { get; }
+
+        public void PrintPerson() 
+        {
+            Console.WriteLine(Person.ToString());
+        }
+
+    }
+
+}
